191ceb4ed0abffd693d22b9e161d566cd35c883a
Andreas Geckler <ageckler@web.de>
2022-11-23 19:03:40 +0100

    commit

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ const button1 = document.querySelector('[data-js="btnCard1"]');
 button1.addEventListener("click", () => {
   button1.classList.toggle("active");
 });
+
+const button2 = document.querySelector('[data-js="btnCard2"');
+button2.addEventListener("click", () => {
+  button2.classList.toggle("active");
+});
+const button3 = document.querySelector('[data-js="btnCard3"]');
+button3.addEventListener("click", () => {
+  button3.classList.toggle("active");
+});
+const button4 = document.querySelector('[data-js="btnCard4"]');
+button4.addEventListener("click", () => {
+  button4.classList.toggle("active");
+});
+const button5 = document.querySelector('[data-js="btnCard5"]');
+button5.addEventListener("click", () => {
+  button5.classList.toggle("active");
+});
+const button6 = document.querySelector('[data-js="btnCard6"]');
+button6.addEventListener("click", () => {
+  button6.classList.toggle("active");
+});
